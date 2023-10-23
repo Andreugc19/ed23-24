da3020424e046125393d0b58540dcba8fc232479
Andreugc19 <agarcia20507@iesjoanramis.org>
2023-10-24 00:41:41 +0200

    Branca Nova

--- HolaGit/src/HolaGit/HolaGit.java
+++ HolaGit/src/HolaGit/HolaGit.java
@@ -5,5 +5,6 @@ public class HolaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo java-git");
+		System.out.println("Nova linia branca");
 	}
 }
