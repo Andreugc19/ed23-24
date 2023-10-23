8ea978b28ac00f68526c4c657018fdc3fed56045
Andreugc19 <agarcia20507@iesjoanramis.org>
2023-10-24 00:13:00 +0200

    Hola Git

---
+++ ControlVersions/HolaWorld.java
@@ -0,0 +1,11 @@
+package mon;
+
+public class HolaWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Acabar de fer
+		/*Començament de text seguit*/
+		System.out.println("Hola Git!");
+	}
+}
